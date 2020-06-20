4c1187cbc947e3a0d98f0212dd9368335dd42de5
sycix <31233910+sycix@users.noreply.github.com>
2020-06-20 18:40:54 +0200

    Add files via upload

---
+++ ApiSample/Controllers/GuestsController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ApiSample.DAL;
+using ApiSample.DTOs.Guests;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiSample.Controllers
+{
+    [Route("api/guests")]
+    [ApiController]
+    public class GuestsController : ControllerBase
+    {
+        private readonly IServiceExample _service;
+        public GuestsController(IServiceExample service)
+        {
+            _service = service;
+        }
+        [HttpGet("test")]
+        public IActionResult Test()
+        {
+            return Ok(_service.Test());
+        }
+        [HttpGet]
+        public IActionResult GetGuests(string lastName)
+        {
+            return Ok(_service.GetGuestsCollection(lastName));
+        }
+        //... api/guests/2
+        [HttpGet("{id:int}")]
+        public IActionResult GetGuest(int id)
+        {
+            return Ok(_service.GetGuestById(id));
+        }
+        [HttpPost]
+        public IActionResult AddGuest(GuestRequestDto requestDto)
+        {
+            if (_service.AddGuest(requestDto))
+                return Ok("new guest created");
+            else
+                return BadRequest("cos poszlo nie tak");
+        }
+        [HttpPut("{id:int}")]
+        public IActionResult UpdateGuest(int id, GuestRequestDto requestDto)
+        {
+            if (_service.UpdateGuest(id, requestDto))
+                return Ok($"guest with id {id} has been updated");
+            else
+                return BadRequest("nie znaleziono goscia");
+        }
+        //....api
+        [HttpDelete("{id:int}")]
+        public IActionResult DeleteGuest(int id)
+        {
+            if (_service.DeleteGuest(id))
+                return Ok($"guest with id {id} has been deleted");
+            else
+                return BadRequest("nie znaleziono goscia");
+        }
+    }
+}
---
+++ ApiSample/DAL/MockDbServiceExample.cs
@@ -0,0 +1,124 @@
+﻿using ApiSample.DTOs.Guests;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+
+namespace ApiSample.DAL
+{
+    public class MockDbServiceExample : IServiceExample
+    {
+        private readonly List<Gosc> Guests;
+        public MockDbServiceExample()
+        {
+            Guests = new List<Gosc>
+            {
+                new Gosc()
+                {
+                    IdGosc = 1,
+                    Imie = "Marek",
+                    Nazwisko = "Chacewicz",
+                    ProcentRabatu = 30
+                },
+                new Gosc()
+                {
+                    IdGosc = 2,
+                    Imie = "Jan",
+                    Nazwisko = "Kowalski"
+                }
+             };
+
+        }
+        public string Test()
+        {
+            return "dziala!!!";
+        }
+
+       public ICollection<GuestResponseDto> GetGuestsCollection(string lastName)
+        {
+            var listToReturn = Guests.Select(x => new GuestResponseDto
+            {
+                IdGuest = x.IdGosc,
+                FirstName = x.Imie,
+                LastName = x.Nazwisko,
+                DiscountPercent = x.ProcentRabatu
+            }).ToList();
+
+            if(!string.IsNullOrEmpty(lastName))         
+                return listToReturn.Where(x => x.LastName == lastName).ToList();
+            else
+                return listToReturn;
+        }
+
+        GuestResponseDto IServiceExample.GetGuestById(int idGuest)
+        {
+            return Guests.Where(x=>x.IdGosc == idGuest)
+                            .Select(x=>new GuestResponseDto
+                         {
+                             IdGuest = x.IdGosc,
+                             FirstName = x.Imie,
+                             LastName = x.Nazwisko,
+                             DiscountPercent = x.ProcentRabatu
+                         }).SingleOrDefault();
+        }
+
+        public bool AddGuest(GuestRequestDto newGuest)
+        {
+            try
+            {
+                var newId = Guests.Max(x => x.IdGosc) + 1;
+                var guestToAdd = new Gosc
+                {
+                    IdGosc = newId,
+                    Imie = newGuest.FirstName,
+                    Nazwisko = newGuest.LastName,
+                    ProcentRabatu = newGuest.DiscountPercent
+                };
+                Guests.Add(guestToAdd);
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public bool UpdateGuest(int id, GuestRequestDto updateGuest)
+        {
+            var guestToUpdate = Guests.SingleOrDefault(x=>x.IdGosc == id);
+            if (guestToUpdate == null)
+                return false;
+            else
+            {
+                guestToUpdate.Imie = updateGuest.FirstName;
+                guestToUpdate.Nazwisko = updateGuest.LastName;
+                guestToUpdate.ProcentRabatu = updateGuest.DiscountPercent;
+                return true;
+            }
+        }
+        public bool DeleteGuest(int id)
+        {
+            var guestToDelete = Guests.SingleOrDefault(x => x.IdGosc == id);
+            if (guestToDelete == null)
+                return false;
+            else
+            {
+                Guests.Remove(guestToDelete);
+                return true;
+            }
+        }
+
+
+        private class Gosc
+        {
+            public int IdGosc { get; set; }
+            public string Imie { get; set; }
+            public string Nazwisko { get; set; }
+            public int ProcentRabatu { get; set; }
+        }
+
+
+    }
+}
